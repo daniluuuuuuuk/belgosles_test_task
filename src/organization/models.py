7acc1de0ec6307df9ed96c0ce3537ccd3e19646a
ardaniluk <artyom.daniluk@gmail.com>
2022-02-23 16:48:12 +0300

    Creating model Organization, Key

--- src/organization/models.py
+++ src/organization/models.py
@@ -1,3 +1,34 @@
+import uuid
+from datetime import datetime, timedelta
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Organization(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255, verbose_name='Название организации')
+
+    class Meta:
+        verbose_name_plural = 'organizations'
+        ordering = ['name']
+
+    def __str__(self):
+        return {self.name}
+
+    def get_absolute_url(self):
+        return reverse('organization', kwargs={'org_id': self.id})
+
+
+class Key(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    key = models.UUIDField(default=uuid.uuid4, editable=True, verbose_name='Ключ')
+    start_date = models.DateField(auto_now_add=True, verbose_name='Начало действия')
+    stop_date = models.DateField(default=datetime.now()+timedelta(days=30), verbose_name='Окончание действия')
+    org_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    is_blocked = models.BooleanField(default=False, verbose_name='Блокировка')
+
+    def __str__(self):
+        return f'Key #{self.key} is related to Organization #{self.org_id}'
+
+    def get_absolute_url(self):
+        return reverse('key', kwargs={'key_id': self.id})
