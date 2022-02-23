abd131cbc282cf39666220ac9d1fc2b78c511467
ardaniluk <artyom.daniluk@gmail.com>
2022-02-23 17:13:39 +0300

    Installing crispy forms and updating settings

--- src/project/settings.py
+++ src/project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'organization.apps.OrganizationConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -134,6 +135,8 @@ STATICFILES_DIRS = []
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 # Default primary key field type
